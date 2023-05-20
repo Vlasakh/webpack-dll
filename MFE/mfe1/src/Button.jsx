e5a07a179bc5041b420a439ebcc3dbb366779658
Vladimir <Zotaper@yandex.ru>
2023-05-20 05:00:51 -0500

    added Prettier

---
+++ MFE/mfe1/src/Button.jsx
@@ -0,0 +1,11 @@
+import React   from "react";
+
+// create a button component
+const Button = ({  onClick }) => (
+<div>
+
+  <button onClick={onClick}>Button from MFE1</button>
+</div>
+);
+
+export default Button;
